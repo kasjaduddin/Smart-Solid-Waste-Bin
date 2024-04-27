63625843656033162cc4e9e7f8e9a845af6a0b7d
kasjaduddin <kasjaduddin@outlook.com>
2024-04-27 16:41:38 +0700

    feat: add subscribe mqtt

---
+++ web/subscriber-mqtt.py
@@ -0,0 +1,22 @@
+import paho.mqtt.client as paho
+import sys
+
+data = ""
+
+def onMessage(client, userdata, msg):
+    data = msg.payload.decode()
+    print(data)
+
+client = paho.Client(paho.CallbackAPIVersion.VERSION1, "segokuning")
+client.on_message = onMessage
+
+if client.connect("localhost", 1883, 60) != 0:
+    print("Could not connect to MQTT Broker!")
+    sys.exit(-1)
+
+client.subscribe("sensor/data")
+
+try:
+    client.loop_forever()
+except:
+    print("Disconnection from broker")

ce099673766c2cd1eaffbe9f31fbae5d7a5a5f28
Farrel Baswara <ahmadfarrelbaswara@mail.ugm.ac.id>
2024-05-01 09:10:16 +0700

    sdh jd

--- iot/segokuning_iot/segokuning_iot.ino
+++ iot/segokuning_iot/segokuning_iot.ino
@@ -2,53 +2,74 @@
 #include <Arduino.h>
 #include <ArduinoJson.h>
 #include "soc/rtc.h"
-// #include "HX711.h"
 #include <WiFi.h> 
 #include <PubSubClient.h>
 #include <Wire.h>
 #include <EasyUltrasonic.h>
+#include <HX711_ADC.h>
 
+// define dht
 #define DHTPIN 33
 #define DHTTYPE DHT11
 DHT dht(DHTPIN, DHTTYPE);
+
+// define hx711
+#if defined(ESP8266)|| defined(ESP32) || defined(AVR)
+#include <EEPROM.h>
+#endif
+const int HX711_dout = 32; //mcu > HX711 dout pin
+const int HX711_sck = 25; //mcu > HX711 sck pin
+HX711_ADC LoadCell(HX711_dout, HX711_sck);
+const int calVal_eepromAdress = 0;
+unsigned long t = 0;
+
+
+// json
 StaticJsonDocument<100> sensor_Doc;
 char buffer[100];
 
-const char* ssid = "uddin";
-const char* password = "12345678";
-const char* mqtt_server = "192.168.34.81";
-
+// wifi & mqtt
+const char* ssid = "BAHAGIA 2,4G";
+const char* password = "kayaberkah21";
+const char* mqtt_server = "broker.hivemq.com";
 WiFiClient espClient;
 PubSubClient client(espClient);
 
 //ultrasonic
 const int trigPin = 26;
 const int echoPin = 27;
-
-// const int LOADCELL_DOUT_PIN = 26;
-// const int LOADCELL_SCK_PIN = 25;
-// long weight;
-// HX711 scale;
 EasyUltrasonic ultrasonic; // Create the ultrasonic object
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Start");
-  dht.begin();
+
   setup_wifi();
   client.setServer(mqtt_server, 1883);
 
   ultrasonic.attach(trigPin, echoPin); // Attaches the ultrasonic sensor on the specified pins on the ultrasonic object
+  dht.begin();
 
-  // rtc_cpu_freq_config_t config;
-  // rtc_clk_cpu_freq_get_config(&config);
-  // rtc_clk_cpu_freq_to_config(RTC_CPU_FREQ_80M, &config);
-  // rtc_clk_cpu_freq_set_config_fast(&config);
+  LoadCell.begin();
+  LoadCell.setReverseOutput(); //uncomment to turn a negative output value to positive
+  float calibrationValue; // calibration value (see example file "Calibration.ino")
+  // calibrationValue = 696.0; // uncomment this if you want to set the calibration value in the sketch
+#if defined(ESP8266)|| defined(ESP32)
+  EEPROM.begin(512); // uncomment this if you use ESP8266/ESP32 and want to fetch the calibration value from eeprom
+#endif
+  EEPROM.get(calVal_eepromAdress, calibrationValue); // uncomment this if you want to fetch the calibration value from eeprom
 
-  // scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  // scale.set_scale(-471.497);                      // this value is obtained by calibrating the scale with known weights; see the README for details
-  //scale.tare();               // reset the scale to 0
-  
+  unsigned long stabilizingtime = 2000; // preciscion right after power-up can be improved by adding a few seconds of stabilizing time
+  boolean _tare = true; //set this to false if you don't want tare to be performed in the next step
+  LoadCell.start(stabilizingtime, _tare);
+  if (LoadCell.getTareTimeoutFlag()) {
+    Serial.println("Timeout, check MCU>HX711 wiring and pin designations");
+    while (1);
+  }
+  else {
+    LoadCell.setCalFactor(calibrationValue); // set calibration value (float)
+    Serial.println("Startup is complete");
+  }
 }
 
 void setup_wifi() {
@@ -100,16 +121,23 @@ void loop() {
   float h = dht.readHumidity();  //humidity
   float t = dht.readTemperature(); // temp
 
+  // LOAD CELL
+  static boolean newDataReady = 0;
+  if (LoadCell.update()) newDataReady = true;
+  float i;
+  if (newDataReady) {
+    i = LoadCell.getData();
+    newDataReady = 0;
+  }
+
   //ULTRASONIC
   float distanceCM = ultrasonic.getPreciseDistanceCM(t, h); // Read the distance in centimeters
 
-  if (isnan(h) && isnan(t) && isnan(distanceCM)) {
+  if (isnan(h) || isnan(t) || isnan(distanceCM) || isnan(i)){
     Serial.println(F("Failed to read from DHT sensor!"));
     return;
   }
 
-  
-  
 
   Serial.print(F("Humidity: "));
   Serial.print(h);
@@ -117,27 +145,20 @@ void loop() {
   Serial.print(t);
   Serial.print(F("°C "));
   Serial.print("Distance (cm): ");
-  Serial.println(distanceCM);
+  Serial.print(distanceCM);
+  Serial.print(" Weight (g): ");
+  Serial.println(i);
 
-  // LOAD CELL
-  // Serial.print(" Weight: ");
-  // Serial.println("podnso");
-  //  weight = scale.get_units(10);
-
-  // Serial.println("pds");
-  // Serial.println(weight);
-
-  // scale.power_down();             // put the ADC in sleep mode
-  // delay(2000);
-  // scale.power_up();
-  // Serial.println("p");
 
   //MQTT
+
   sensor_Doc["Temperature"] = t;
   sensor_Doc["Humidity"] = h;
   sensor_Doc["Distance"] = distanceCM;
+  sensor_Doc["Weight"] = i;
   serializeJson(sensor_Doc, buffer);
   client.publish("sensor/data", buffer);
+  
   char tempString[8];
   dtostrf(t, 1, 2, tempString);
   client.publish("segokuningteam/temp", tempString);
@@ -150,7 +171,7 @@ void loop() {
   dtostrf(distanceCM, 1, 2, distanceString);
   client.publish("segokuningteam/distance", distanceString);
 
-  // char weightString[8];
-  // dtostrf(weight, 1, 2, weightString);
-  // client.publish("segokuningteam/weight", weightString);
+  char weightString[8];
+  dtostrf(i, 1, 2, weightString);
+  client.publish("segokuningteam/weight", weightString);
 }
